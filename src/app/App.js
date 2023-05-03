a45140a94449e62647948e56c0f3c6d119533f24
IS <mr.issarapong@gmail.com>
2023-05-03 19:40:35 +0700

    setup default

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import '../app/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Hello world</h3>
+ 
+      </header>
+    </div>
+  );
+}
+
+export default App;
