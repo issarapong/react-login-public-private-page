27acf1f768ae62f98f1cc98be2df5f1e8e53b38b
IS <1588186+issarapong@users.noreply.github.com>
2023-05-06 22:01:38 +0700

    Finish:Simple profile page

--- src/app/App.js
+++ src/app/App.js
@@ -2,11 +2,13 @@ import '../app/App.css';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage'
 import HomePage from '../pages/HomePage'
+import ProfilePage from '../pages/ProfilePage'
 function App() {
   return (
    //<LoginPage />
      //<RegisterPage />
-     <HomePage />
+     //<HomePage />
+     <ProfilePage />
 
    
   
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ProfilePage() {
+  return (
+    <div className='container'>
+     <div className='profile'>
+    <img src="https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY" />
+    <p>name</p>
+    </div>
+    </div>
+  )
+}
+
+export default ProfilePage
