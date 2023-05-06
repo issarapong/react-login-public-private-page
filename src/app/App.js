5a31139343e8cf8f9f88880ecec27798119d29cb
IS <1588186+issarapong@users.noreply.github.com>
2023-05-06 22:04:12 +0700

    Finish:Simple dashboard

--- src/app/App.js
+++ src/app/App.js
@@ -3,13 +3,15 @@ import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage'
 import HomePage from '../pages/HomePage'
 import ProfilePage from '../pages/ProfilePage'
+import DashBoardPage from '../pages/DashBoardPage';
+
 function App() {
   return (
    //<LoginPage />
      //<RegisterPage />
      //<HomePage />
-     <ProfilePage />
-
+     //<ProfilePage />
+        <DashBoardPage />
    
   
    );
---
+++ src/pages/DashBoardPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashBoardPage() {
+  return (
+    <div>Welcome DashBoardPage, Now Private!!</div>
+  )
+}
+
+export default DashBoardPage
